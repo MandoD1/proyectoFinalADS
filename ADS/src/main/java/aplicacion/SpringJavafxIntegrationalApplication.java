e19a32562e0b4c4b5bdc13c29c46336d5198fbfa
mandod1 <damodmo1110@gmail.com>
2025-11-23 01:11:06 -0500

    pequeños cambios para javafx

---
+++ ADS/src/main/java/aplicacion/SpringJavafxIntegrationalApplication.java
@@ -0,0 +1,38 @@
+package aplicacion;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class SpringJavafxIntegrationalApplication extends Application {
+
+    public static ConfigurableApplicationContext applicationContext;
+    public static Parent rootNode;
+    public static Stage stage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/tipologin.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Sistema de Gestión Académica");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
